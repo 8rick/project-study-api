d1f16d138724855bf16c75aef91e11ea1c97715d
Rickelmy Barbosa da Silva <barbosarickelmy8@gmail.com>
2026-03-26 20:42:27 -0300

    feat: Middlewares of auth and projectsRoutes test.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import fastifyCors from "@fastify/cors";
 import Fastify from "fastify";
 import { authRoutes } from "./modules/auth/routes/authRoutes";
+import { projectsRoutes } from "./modules/projects/routes/projectsRoutes";
 
 const app = Fastify();
 
 app.register(authRoutes);
+app.register(projectsRoutes);   
 
 
 app.listen({ port: 3333}).then(() => {
