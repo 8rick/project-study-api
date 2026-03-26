1b15d741c7f1e464f0b3550ff6880224780925b1
Rickelmy Barbosa da Silva <barbosarickelmy8@gmail.com>
2026-03-26 10:42:08 -0300

    feat: authRoutes and corretion bug and test route.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import fastifyCors from "@fastify/cors";
 import Fastify from "fastify";
+import { authRoutes } from "./modules/auth/routes/authRoutes";
 
 const app = Fastify();
 
-app.register(fastifyCors);
+app.register(authRoutes);
 
 app.get("/", async () => {
-    return {message: "Api funcionando"}
+  return { message: "API funcionando 🚀" }
 })
 
+
 app.listen({ port: 3333}).then(() => {
-    return("Server running on http://locallhost:3333")
+    return("Server running on port http://locallhost:3333")
 })
