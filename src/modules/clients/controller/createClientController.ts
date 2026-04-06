b8e8fa066912b73ce1946b1a3b5a0410df3229c6
Rickelmy Barbosa da Silva <barbosarickelmy8@gmail.com>
2026-04-06 19:41:39 -0300

    feat: controller crateClient.

---
+++ src/modules/clients/controller/createClientController.ts
@@ -0,0 +1,14 @@
+import { createClientSchema } from "../schemas/createClientSchema";
+import { createClientService } from "../service/createClientService";
+
+
+export async function creatClientController(request, reply) {
+
+    const userId = request.user.userId;
+
+    const data = createClientSchema.parse(request.body);
+
+    const client = await createClientService(data, userId);
+
+    return reply.status(201).send(client);
+}
